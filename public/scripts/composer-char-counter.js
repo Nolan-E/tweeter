d6985ed1eaad0944f68a7dd1002ce2b4a59092fc
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-23 16:26:58 -0700

    Update composer-char-counter.js with functional counter. Add #limit-exceed style to new-tweet.css for red color over character limit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,13 @@
-$(document).ready(() => {
-  
+$(document).ready(function() {
+  $("#tweet-text").on("keyup", function(event) {
+    const tweetLength = $(this).val().length;
+    const charCounter = $(this).closest("form").find(".counter");
+    const currCount = 140 - tweetLength;
+    if (currCount < 0) {
+      charCounter.attr('id', 'limit-exceed');
+    } else {
+      charCounter.removeAttr('id');
+    }
+    charCounter.html(currCount);
+  });
 });
