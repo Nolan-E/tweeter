487f4d127027cd241e669ecfeaff0e5dd2917e10
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-23 16:56:31 -0700

    Refactor counter listener function. Add html skeleton for new tweet container.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,15 @@
 $(document).ready(function() {
-  $("#tweet-text").on("keyup", function(event) {
-    const tweetLength = $(this).val().length;
-    const charCounter = $(this).closest("form").find(".counter");
-    const currCount = 140 - tweetLength;
-    if (currCount < 0) {
-      charCounter.attr('id', 'limit-exceed');
-    } else {
-      charCounter.removeAttr('id');
-    }
-    charCounter.html(currCount);
-  });
+  $("#tweet-text").on("keyup", counterUpdater);
 });
+
+const counterUpdater = function() {
+  const tweetLength = $(this).val().length;
+  const charCounter = $(this).closest("form").find(".counter");
+  const currCount = 140 - tweetLength;
+  if (currCount < 0) {
+    charCounter.attr('id', 'limit-exceed');
+  } else {
+    charCounter.removeAttr('id');
+  }
+  charCounter.html(currCount);
+};
