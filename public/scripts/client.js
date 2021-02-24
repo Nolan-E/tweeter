b2be135fdb1661f1c4d055f40f039d32f9ce47d6
Nolan Eckert <nolan.eckert@gmail.com>
2021-02-24 14:52:08 -0700

    Add days ago timestamp calculation. Add class to avatar img for right margin again.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,6 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// TEST DATA
 const data = [
   {
     "user": {
@@ -15,7 +16,7 @@ const data = [
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1461116232227
+    "created_at": 1614203156000
   },
   {
     "user": {
@@ -25,22 +26,36 @@ const data = [
     "content": {
       "text": "Je pense , donc je suis"
     },
-    "created_at": 1461113959088
+    "created_at": 1613199600000
   }
 ];
 
+// IMPLEMENT FUNCTIONS
+// Calculate days ago
+const daysAgo = function(createdTimestamp) {
+  let output;
+  const timeAgo = Math.floor(((Date.parse(new Date) - createdTimestamp) / 1000) / (3600 * 24));
+  if (timeAgo === 1) {
+    return output = `${timeAgo} day ago`;
+  } else if (timeAgo < 1) {
+    return output = `today`;
+  }
+  return output = `${timeAgo} days ago`;
+};
+
+// create DOM elements
  const createTweetElement = function(tweetObj) {
   const $tweet = $(`<article class="tweets-feed">
   <header class="tweets-feed-header">
     <div class="tweets-feed-name">
-      <img src="${tweetObj.user.avatars}"> ${tweetObj.user.name}
+      <img class="tweets-feed-user" src="${tweetObj.user.avatars}"> ${tweetObj.user.name}
     </div>
     <span class="tweets-feed-handle">${tweetObj.user.handle}</span>
   </header>
   <div class="tweets-feed-body">
     <p class="tweets-feed-text">${tweetObj.content.text}</p>
   </div>
-  <footer class="tweets-feed-data">${tweetObj.created_at}
+  <footer class="tweets-feed-data">${daysAgo(tweetObj.created_at)}
     <div>
       <span class="tweets-feed-flag"><i class="fas fa-flag"></i></span> <span class="tweets-feed-retweet"><i class="fas fa-retweet"></i></span><span class="tweets-feed-heart"><i class="fas fa-heart"></i></span>
     </div>
@@ -49,6 +64,7 @@ const data = [
   return $tweet;
 };
 
+// render tweet from database object into DOM elements
 const renderTweets = function(tweetArr) {
   for (const tweet of tweetArr) {
     const tweetToAppend = createTweetElement(tweet);
@@ -56,6 +72,7 @@ const renderTweets = function(tweetArr) {
   }
 };
 
+// doc ready check with function invocation
 $(document).ready(function() {
   renderTweets(data);
 
